3300a3a85c4343c56e0f0bf1cdd27d0142a42db5
Adrian <adrianshisia@gmail.com>
2023-06-19 12:39:12 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 // Code your solutions in this file
+function writeCards(names) {
+  const thankYouMessages = ["Thank you, Guadalupe, for the wonderful surprise gift!"]
 
-
-let friends = ["Guadalupe", "Ollie", "Aki"]
-function writeCards(friends, event) {
-    for(let i = 0; i < friends.length; i++) {
-        console.log (`"Thank you, ${friends[i]}, for the wonderful ${event} gift!"`)
+    for (let i = 0; i < names.length; i++) {
+      const name = [Ollie,]
+      const message = `Thank you, Ollie, for the wonderful surprise gift!`
+      thankYouMessages.push(message)
     }
-
-    return friends
+    return thankYouMessages
 }
-writeCards(friends, "surprise")
- 
- function countDown() {
-    let number = 10;
-    while (number < 10) {
-      console.log (number--)
-    }
- }
- 
+
+
+function countDown(number){
+  for (let i = number; i >= 0; i--){
+    console.log(i)
+  }
+}
